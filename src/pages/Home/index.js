85806a965eae88ecc9dc1a489367a1531703d2e9
kannadadr <kanandar43@gmail.com>
2023-10-24 15:08:40 -0300

    instalado biblioteca styled components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,27 @@
-function Home(){
-    return(
-        <h1>Hello, World</h1>
+function Home() {
+    return (
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
+                    <span>Spider Man</span>
+
+                </li>
+                <li>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
+                    <span>Spider Man</span>
+
+                </li>
+                <li>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
+                    <span>Spider Man</span>
+
+                </li>
+
+            </ul>
+
+        </div>
     )
 
 }
