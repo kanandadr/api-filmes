bf92acaaf486804739557790b509419633262da6
kannadadr <kanandar43@gmail.com>
2023-10-27 17:10:21 -0300

    alterações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,71 @@
+import { Container, MovieList, Movie } from "./styles";
+
+
 function Home() {
+
+    const movies = [
+        {
+            Id:1,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+
+        },
+        {
+            id:2,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+        
+
+        },
+        {
+            id:3,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+    
+
+        },
+        {
+            Id:1,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+
+        },
+        {
+            id:2,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+        
+
+        },
+        {
+            id:3,
+            titile: 'Spaider Man',
+            Image_url: "https://tse4.mm.bing.net/th?id=OIP.M73I1Mw0rSxA7f9PCdGJpAHaLH&pid=Api&P=0&h=180"
+    
+
+        }
+    ]
+
     return (
-        <div>
+
+        <Container>
             <h1>Movies</h1>
-            <ul>
-                <li>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
-                    <span>Spider Man</span>
+            <MovieList>
 
-                </li>
-                <li>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
-                    <span>Spider Man</span>
+                {movies.map(movie => {
+                    return (
+                        <Movie key ={movie.id}>
+                            <a href="https://google.com.br"><img src={movie.Image_url} alt="movie.title" /></a>
+                            <span>{movie.titile}</span>
 
-                </li>
-                <li>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiCF7TkDaxIKd5YkVvUL_QJZ-K6uqaZX6GDg&usqp=CAU" alt="Spider Man" />
-                    <span>Spider Man</span>
+                        </Movie>
 
-                </li>
+                    )
+                })}
 
-            </ul>
+            </MovieList>
 
-        </div>
+        </Container>
     )
 
 }
